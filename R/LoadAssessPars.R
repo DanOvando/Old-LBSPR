5b97736d2e10931c6f12f2b94e6146ea6a8416be
Adrian-2013 <ar.hordyk@gmail.com>
2015-05-13 09:33:43 +0800

    Updated function to download admb model

--- R/LoadAssessPars.R
+++ R/LoadAssessPars.R
@@ -19,7 +19,7 @@
 #' @export
 
 
-LoadAssessPars <- function(PathtoAssessFile="~/PathToAssessFile", AssessParFileName="AssessPars", AssessParExt=".csv", ind=1, LenMids) {
+LoadAssessPars <- function(PathtoAssessFile="~/PathToAssessFile", AssessParFileName="AssessPars", AssessParExt=".csv", ind=1, LenMids, LHR_OverRide=list()) {
   
   OptimiseFitness <- function(logMslope, SimPars, Function) {
     Mslope <- exp(logMslope) #+ 0.000000001
@@ -27,45 +27,61 @@ LoadAssessPars <- function(PathtoAssessFile="~/PathToAssessFile", AssessParFileN
     return(Function(SimPars=SimPars)$ObjFun)
   }
   if(AssessParExt == ".csv") {
-    Dat <- read.csv(file.path(PathtoAssessFile, paste0(AssessParFileName, AssessParExt)))
+    Dat <- read.csv(file.path(PathtoAssessFile, paste0(AssessParFileName, AssessParExt)), as.is=TRUE)
     row.names(Dat) <- Dat[,1]
     
     # Load new parameters 
-    MK     <- Dat["MK",ind+1]
-    Linf   <- Dat["Linf",ind+1]
-    CVLinf <- Dat["CVLinf",ind+1]
-    L50    <- Dat["L50",ind+1]
-    L95    <- Dat["L95",ind+1]
-    Walpha <- Dat["Walpha",ind+1]
-    Wbeta  <- Dat["Wbeta",ind+1]
-    FecB   <- Dat["FecB",ind+1]
-    Mpow   <- Dat["Mpow",ind+1]
-    NGTG   <- Dat["NGTG",ind+1]
-    SDLinf <- CVLinf * Linf
-    MaxSD  <- Dat["MaxSD",ind+1]
-    GTGLinfdL <- ((Linf + MaxSD * SDLinf) - (Linf - MaxSD * SDLinf))/(NGTG-1);	
-	
-    SL50Min <- Dat["SL50Min", ind+1]
+	MK     <- LHR_OverRide$MK
+	Linf   <- LHR_OverRide$Linf 
+	CVLinf <- LHR_OverRide$CVLinf
+	L50    <- LHR_OverRide$L50
+	L95    <- LHR_OverRide$L95
+	Walpha <- LHR_OverRide$Walpha
+	Wbeta  <- LHR_OverRide$Wbeta
+	FecB   <- LHR_OverRide$FecB
+	Mpow   <- LHR_OverRide$Mpow
+	NGTG   <- LHR_OverRide$NGTG
+
+    if (is.null(MK)) 	MK   <- as.numeric(Dat["MK",ind+1])
+    if (is.null(Linf)) 	Linf   <- as.numeric(Dat["Linf",ind+1])
+    if (is.null(CVLinf))CVLinf <- as.numeric(Dat["CVLinf",ind+1])
+    if (is.null(L50)) 	L50    <- as.numeric(Dat["L50",ind+1])
+    if (is.null(L95)) 	L95    <- as.numeric(Dat["L95",ind+1])
+    if (is.null(Walpha))Walpha <- as.numeric(Dat["Walpha",ind+1])
+    if (is.null(Wbeta)) Wbeta  <- as.numeric(Dat["Wbeta",ind+1])
+    if (is.null(FecB)) 	FecB   <- as.numeric(Dat["FecB",ind+1])
+    if (is.null(Mpow)) 	Mpow   <- as.numeric(Dat["Mpow",ind+1])
+    if (is.null(NGTG)) 	NGTG   <- as.numeric(Dat["NGTG",ind+1])
+
+    SDLinf <- as.numeric(CVLinf * Linf)
+    MaxSD  <- as.numeric(Dat["MaxSD",ind+1])
+    GTGLinfdL <- ((Linf + MaxSD * SDLinf) - (Linf - MaxSD * SDLinf))/(NGTG-1)
+  
+	DiffLinfs <- seq(from=Linf-MaxSD*SDLinf, to=Linf+MaxSD*SDLinf, length=NGTG)
+
+    SL50Min <- as.numeric(Dat["SL50Min", ind+1])
     if (length(SL50Min) < 1 | is.na(SL50Min)) SL50Min <- 1
-    SL50Max <- Dat["SL50Max", ind+1]
+    SL50Max <- as.numeric(Dat["SL50Max", ind+1])
     if (length(SL50Max) < 1| is.na(SL50Max)) SL50Max <- 0.95 * Linf
-    DeltaMin <- Dat["DeltaMin", ind+1]
+    DeltaMin <- as.numeric(Dat["DeltaMin", ind+1])
     if (length(DeltaMin) < 1| is.na(DeltaMin)) DeltaMin <- 0.01
-    DeltaMax <- Dat["DeltaMax", ind+1]
+    DeltaMax <- as.numeric(Dat["DeltaMax", ind+1])
     if (length(DeltaMax) < 1| is.na(DeltaMax)) DeltaMax <- 0.5 * Linf
     
     AssessPars <- list(MK=MK, Linf=Linf, CVLinf=CVLinf, SDLinf=SDLinf, L50=L50, L95=L95, 
                     Walpha=Walpha, Wbeta=Wbeta, FecB=FecB, Mpow=Mpow, 
                     NGTG=NGTG, GTGLinfdL=GTGLinfdL, MaxSD=MaxSD, SL50Min=SL50Min, 
-                    SL50Max=SL50Max, DeltaMin=DeltaMin, DeltaMax=DeltaMax)
+                    SL50Max=SL50Max, DeltaMin=DeltaMin, DeltaMax=DeltaMax, DiffLinfs=DiffLinfs)
 	
-	AssessPars$Linc <- LenMids[2] - LenMids[1]
+	AssessPars$LenMids <- LenMids
+	AssessPars$Linc <- BY <- LenMids[2] - LenMids[1]
+	AssessPars$LenBins <- seq(from=LenMids[1] - 0.5*BY, by=BY, length=length(LenMids)+1)
 	AssessPars$AssessOpt <- TRUE
+
 	AssessPars$Mslope <- exp(optimise(OptimiseFitness, interval=log(c(0.000001, 0.1)), SimPars=AssessPars, Function=SimMod_LHR)$minimum) # Run Sim Model and fit optimal MSlope
   }
   
   if(AssessParExt != ".csv") stop("Unrecognized file extension")
   print("Assessment parameters successfully loaded")
-  print(as.data.frame(AssessPars))
   return(AssessPars)
 }
--- R/downloadADMB.r
+++ R/downloadADMB.r
@@ -1,21 +1,21 @@
-#' Function to download the LBSPR binary from GitHub and copy to local machine.
+#' Function to download the LBSPR binary from online repository and copy to local machine.
 #' @name CopyADMBFile  
 #' @title Download LBSPR executable. 
 #' @param System A \code{character} string of either "Win" for Windows OS compiled executable, or anything else (not blank) for UNIX OS
 #' @param DestPath A code{character} string detailing the full path where the binary file is to be downloaded.
-#' @param vers A \code{character} string representing the version. Default value should be the latest release. 
 #' @author Adrian Hordyk 
 #' @export
 
-CopyADMBFile <- function(System="Win", DestPath=NULL, vers="1.00") {
+CopyADMBFile <- function(System="Win", DestPath=NULL) {
   if (System == "Win") {
     destPath <- paste0(DestPath, "/lbspr.exe")
-	URL <- paste0("https://github.com/AdrianHordyk/LBSPR_ADMB/releases/download/v", vers, "/lbspr.exe")
-    download(URL, destPath)
+	
+	URL <- paste0("https://dl.dropboxusercontent.com/u/24856730/LBSPR/release/current/lbspr.exe")
+    download(URL, destPath, mode="wb")
   }
   if (System != "Win") {
     destPath <- paste0(DestPath, "/lbspr")
-	URL <- paste0("https://github.com/AdrianHordyk/LBSPR_ADMB/releases/download/v", vers, "/lbspr")
-    download(URL, destPath)
+	URL <- paste0("https://dl.dropboxusercontent.com/u/24856730/LBSPR/release/current/lbspr")
+    download(URL, destPath, mode="wb")
   }  
 }
